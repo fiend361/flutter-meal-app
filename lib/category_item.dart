012411cdb12f97d6d5d6f7baa94f24a9940140e4
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2021-03-18 20:12:18 +0200

    forwarded category item data to category screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
   CategoryItem(
+    this.id,
     this.title,
     this.color,
   );
@@ -14,7 +16,7 @@ class CategoryItem extends StatelessWidget {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryMealsScreen();
+          return CategoryMealsScreen(id, title);
         },
       ),
     );
